bab68d3c255c605fa4bf6ed56746829f9f553b6d
paras123yadav <54791137+paras123yadav@users.noreply.github.com>
2020-04-17 23:43:33 +0530

    Update h.c

--- DSlab/h.c
+++ DSlab/h.c
@@ -1,122 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
-struct node
-{
+struct tree{
     int data;
-    struct node *left;
-    struct node *right;
-}*root;
-void preorder(struct node *root)
-{
-    if (root==0)
-    {
-        return;
-    }
-    printf("%d", root->data);
-    preorder(root->left);
-    preorder(root->right);
-    
+    struct tree *left;
+    struct tree *right;
+};
+struct tree *newNode(int data){
+    struct tree *node = (struct tree*)malloc(sizeof(struct tree));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
 }
-void inorder(struct node *root)
-{
-    if (root==NULL)
-    {
-        return ;
-    }
-    else
-    {
-        inorder(root->left);
-        printf("%d",root->data);
-        inorder(root->right);
-    }
+struct stack{
+    struct tree *t;
+    struct stack *next;
+};
+void push(struct stack **stack,struct tree *tree){
+    struct stack *newTree = (struct stack*) malloc (sizeof(struct stack));
+    newTree->t = tree;
+    newTree->next = (*stack);
+    (*stack) = newTree;
 }
-void postorder(struct node *root)
-{
-    if (root==0)
-    {
-        return;
-    }
-    else
-    {   
-        postorder(root->left);
-        postorder(root->right);
-        printf("%d",root->data);
-    }
+int isEmpty(struct stack *stack){
+    return (stack==NULL);
 }
-int height(struct node *root)
-{
-    int l,r;
-    if (root==NULL)
-    {
-        return 0;
-    }
-    else if (root->left==NULL&&root->right==NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        l=height(root->left);
-        r=height(root->right);
-        return 1+(l>r?l:r);
-    }
+struct tree *pop(struct stack** stack){
+    struct stack *temp;
+    struct tree *root;
+    temp = *stack;
+    root = temp->t;
+    *stack = temp->next;
+    free(temp);
+    return root;
 }
-void main()
-{
-    int i,data,n;
-    struct node *temp,*temp1;
-    printf("enter number of nodes you want");
-    scanf("%d",&n);
-    for ( i = 0; i <n ; i++)
-    {
-        temp1=(struct node *)malloc(sizeof(struct node));
-        printf("enter the data for node");
-        scanf("%d",&data);
-        temp1->data=data;
-        temp1->left=NULL;
-        temp1->right=NULL;
-        if (i==0)
-        {
-            root=temp1;
+void InOrder(struct tree *root){
+    struct tree* curr = root;
+    struct stack *s = NULL;         
+    int flag = 0;
+    while(!flag){
+        if(curr != NULL){
+            push(&s,curr);          
+            curr = curr->left;     
         }
-        else
-        {
-            temp=root;
-            while(1)
-            {
-                if (temp1->data>temp->data)
-                {
-                    if (temp->right==NULL)
-                    {
-                        temp->right=temp1;
-                        break;
-                    }
-                    else
-                    {
-                        temp=temp->right;
-                    }
-                }
-                else
-                {
-                    if (temp->left==NULL)
-                    {
-                        temp->left=temp1;
-                        break;
-                    }
-                    else
-                    {
-                        temp=temp->left;
-                    }
-                }
+        else{
+            if(!isEmpty(s)){
+                curr = pop(&s);     
+                printf("%d\t",curr->data);      
+                curr = curr->right;         
+            }
+            else{
+                flag = 1;
             }
         }
     }
-    printf("\nthe preorder traversal of the tree :");
-    preorder(root);
-    printf("\nthe inorder traversal of this tree :");
-    inorder(root);
-    printf("\nthe postorder traversal of the tree :");
-    postorder(root);
-    printf("height of the tree is:%d",height(root));
 
+}
+int height(struct tree *root){
+    if(root == NULL){
+        return 0;
+    }
+    else{
+        int lst;
+        int rst;
+        lst = height(root->left);
+        rst = height(root->right);
+        if(rst>lst){
+            return rst+1;
+        }else{
+            return lst+1;
+        }
+    }
+}
+int main(){
+    struct tree *root = newNode(2);
+    root->left = newNode(5);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(1);
+    root->right->left = newNode(8);
+    printf("InOrder Tree Traversal without recursion:\n");
+    InOrder(root);
+    printf("\n Height of Tree is = %d\n",height(root));
+    return 0;
 }
